f0d37137ed409ec1fbd6575ed103d73ce9b7810f
traktorsem <traktorsem996@gmail.com>
2019-12-11 12:45:43 +0200

    Init project Header component Global structure

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { HomePage } from './home.page';
+import {HeaderComponent} from "../components/header/header.component";
+import {LoginScreenComponent} from "../components/login-screen/login-screen.component";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage, HeaderComponent, LoginScreenComponent]
+})
+export class HomePageModule {}
