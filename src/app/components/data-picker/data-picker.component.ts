0e001da8b967c427bf4c95047a9d8610647d22e2
traktorsem <traktorsem996@gmail.com>
2019-12-23 13:58:22 +0200

    Date input

--- src/app/components/data-picker/data-picker.component.ts
+++ src/app/components/data-picker/data-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-data-picker',
@@ -7,16 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataPickerComponent implements OnInit {
   _show: boolean = false;
+  @ViewChild('dateInput', null) date: any;
+  @ViewChild('timeInput', null) time: any;
+
+  _todayDate = new Date(  Date.now() - (Date.now() % (1000 * 60 * 60 * 24)) ).toISOString();
+  get todayDate() {
+    return this._todayDate;
+  }
+  set todayDate(v) {
+    console.warn(v);
+    this._todayDate = v;
+  }
+
   constructor() { }
 
   ngOnInit() {}
 
-
   show() {
     this._show = true;
+    console.log(this.todayDate);
   }
   hide() {
     this._show = false;
   }
-}
 
+  inputDate() {
+    this.date.el.click();
+  }
+  inputTime() {
+    this.time.value = this.date.value;
+    this.time.el.click();
+  }
+  selectedDate() {
+    console.log(this.time.value);
+  }
+
+
+  pickerOptions = {
+    cssClass: 'data-picker',
+    mode: 'ios'
+  };
+  get datePickerOptions() {
+    return {
+      ...this.pickerOptions,
+      buttons: [
+        {},
+        {
+          text: 'Done',
+          handler: this.inputTime.bind(this)
+        }
+      ]
+    }
+  }
+}
